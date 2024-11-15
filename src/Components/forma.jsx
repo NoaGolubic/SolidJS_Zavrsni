96bac86e2dfe97a41278ed5248c1ccb2ca4e8109
Noa Golubic <noagolubic030606@gmail.com>
2024-11-15 19:58:53 +0100

    skaliranje za mobilne uredjaje, poboljsanje dizajna

--- src/Components/forma.jsx
+++ src/Components/forma.jsx
@@ -4,6 +4,9 @@ import "../CSS/forma.css";
 const forma = () => {
   return (
       <>
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </head>
       <div class="glass-container2">
         <div className="pozadina">
           <h1>Forma za prikaz informacija</h1>
