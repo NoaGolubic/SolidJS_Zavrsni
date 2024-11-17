4e1dee16fa53c9a9445c2613314c5f47c835ba07
Noa Golubic <noagolubic030606@gmail.com>
2024-11-17 18:22:41 +0100

    ZAVRŠENO SKALIRANJE, PORAVNAT PO POTREBI

--- src/Pages/info.jsx
+++ src/Pages/info.jsx
@@ -11,22 +11,7 @@ const info = () => {
     <div className="kartica-wrap">
         <Navbar showWelcome={false} />
       <div className="glass-kartica1">
-        <h3 className="api">O autorima</h3>
-        <p className="papi">
-          Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i
-          slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski
-          standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo
-          tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom
-          tiska. Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u
-          svijet elektronskog slovoslagarstva, ostajući u suštini nepromijenjen.
-          Postao je popularan tijekom 1960-ih s pojavom Letraset listova s
-          odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno
-          izdavaštvo kao što je Aldus PageMaker koji također sadrži varijante
-          Lorem Ipsum-a.
-        </p>
-      </div>
-      <div className="glass-kartica2">
-        <h3 className="modeli">Licence i korištena sredstva</h3>
+        <h3 className="nas">O autorima</h3>
         <p className="pmodeli">
           Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i
           slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski
@@ -41,7 +26,7 @@ const info = () => {
         </p>
       </div>
       <div className="glass-kartica2">
-        <h3 className="modeli">O PlaneSpotteru</h3>
+        <h3 className="nas">Licence i korištena sredstva</h3>
         <p className="pmodeli">
           Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i
           slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski
@@ -56,7 +41,22 @@ const info = () => {
         </p>
       </div>
       <div className="glass-kartica2">
-        <h3 className="modeli">Kontaktirajte nas!</h3>
+        <h3 className="nas">O PlaneSpotteru</h3>
+        <p className="pmodeli">
+          Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i
+          slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski
+          standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo
+          tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom
+          tiska. Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u
+          svijet elektronskog slovoslagarstva, ostajući u suštini nepromijenjen.
+          Postao je popularan tijekom 1960-ih s pojavom Letraset listova s
+          odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno
+          izdavaštvo kao što je Aldus PageMaker koji također sadrži varijante
+          Lorem Ipsum-a.
+        </p>
+      </div>
+      <div className="glass-kartica2">
+        <h3 className="nas">Kontaktirajte nas!</h3>
         <p className="pmodeli">
           Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i
           slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski
